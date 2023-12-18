618306214a6755621deab59606b8c70ae2bdbc1c
michaelcao28 <47520436+michaelcao28@users.noreply.github.com>
2023-12-18 17:55:30 +1100

    Update tips_for_nicer_plots.py

--- tips_for_nicer_plots.py
+++ tips_for_nicer_plots.py
@@ -1,4 +1,19 @@
+# significantly improves image quality
 %config InlineBackend.figure_format = 'retina'
 
-plt.style.available # see list of available styles
+# see styles and change style
+plt.style.available 
 plt.style.use(['seaborn-v0_8-colorblind', 'seaborn-v0_8-darkgrid'])
+
+# view and change colour cycle
+from cycler import cycler
+plt.rcParams['axes.prop_cycle']
+current_cycler = plt.rcParams['axes.prop_cycle']
+colors = [c['color'] for c in current_cycler]
+new_color_order = colors[::-1] # reversed order
+
+# change colour cycle globally
+plt.rcParams['axes.prop_cycle'] = cycler(color=new_color_order)
+
+# change colour cycle only for a particular plot
+ax.set_prop_cycle(cycler(color=new_color_order))
